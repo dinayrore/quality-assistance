c983766bb8c889de0a7d29ccc500deda2ceaaa9a
Kristine Teichmann <kteichmann@w-rl-ml-kteichm.local>
2018-03-29 20:56:10 -0400

    updated routes to include validating page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/email-requirements', to: 'pages#requirements'
   get '/dm-requirements', to: 'pages#requirements'
+  get '/validating', to: 'pages#validation'
 end

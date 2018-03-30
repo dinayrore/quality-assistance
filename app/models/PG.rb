202f05117a6700570a7b94ca297703263c2f98f7
Kristine Teichmann <kteichmann@w-rl-ml-kteichm.local>
2018-03-29 21:38:17 -0400

    Added relationships to models.

--- app/models/PG.rb
+++ app/models/PG.rb
@@ -1,14 +1,4 @@
-require 'roo'
-
 class PG
-  def initialize
-    @workbook = Roo::Spreadsheet.open("#{ENV["HOME"]}/Desktop/QA/Programming_Grid.xlsx")
-    # if current_page?(email_requirements_path)
-    #   @mvs =
-    # else
-    #   @mvs =
-    # end
-    #
-    # @mvs.
-  end
+  has_one :workbook
+  has_many :worksheets
 end

aca572aff40d571dd5233b67cb3470d0a1d71b51
Kristine Teichmann <kteichmann@w-rl-ml-kteichm.local>
2018-03-29 21:40:12 -0400

    Updated routes for new worksheets path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get '/email-requirements', to: 'pages#requirements'
-  get '/dm-requirements', to: 'pages#requirements'
+  get '/email-pg-worksheet', to: 'pages#worksheet'
+  get '/dm-pg-worksheet', to: 'pages#worksheet'
   get '/validating', to: 'pages#validation'
 end

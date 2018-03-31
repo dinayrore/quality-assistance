539d4c4abca80121995de72c2ade133a37774475
Kristine <kteich88@gmail.com>
2018-03-30 23:18:15 -0400

    updated QA class.

--- qa.rb
+++ qa.rb
@@ -2,6 +2,7 @@
 require 'optparse'
 require 'ostruct'
 require 'pry'
+require_relative 'DM'
 
 class QA
 
@@ -11,29 +12,34 @@ class QA
 
     # The options specified in CLI will be collected in *options*.
     # Set default option values here.
-    options = Hash.new
+    options = OpenStruct.new
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "How to: quality_assistance.rb [options]"
 
       opts.separator ""
-      opts.separator "QA options:"
+      opts.separator "Define QA type and required files:"
 
-      # Optional arguments & multi-line descriptions
-      opts.on('-e', '--email FILEPATH', 'Analyze Email QA') do |email|
-        options[:email] = email
+      # List of arguments.
+      opts.on("--email TYPE,FILEPATH,FILEPATH,FILEPATH", Array,
+              "A 'list' of arguments",
+              "  (TYPE = Email)",
+              "  (FILEPATH 'pg.xlsx', '/*.eml', 'QA.csv' )") do |list|
+        options.list = list
       end
 
-      opts.on('-d', '--direct mail FILEPATH',
-              'Analyze Direct Mail QA',
-              ' (PDF text & images)') do |dm|
-        options[:direct_mail] = dm
+      # List of arguments.
+      opts.on("--dm TYPE,FILEPATH,FILEPATH,FILEPATH", Array,
+              "A 'list' of arguments",
+              "  (TYPE = DM)",
+              "  (FILEPATH 'pg.xlsx', '*.pdf', '*.txt')") do |list|
+        options.list = list
       end
+
 
       opts.separator ""
-      opts.separator "Common options:"
 
-      opts.on("-h", "--help", "Prints help options") do
+      opts.on("-h", "--help", "Prints options menu") do
         puts opts
         exit
       end
@@ -45,7 +51,9 @@ class QA
   end
 
   def self.start(options)
+    Email.new(options) if options.qa == "email"
     binding.pry
+    DM.new(options) if options.list[0].include?("dm")
   end
 
 end

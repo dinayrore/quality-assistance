e9330c0b699ca2f00bd2d20c145879ea0fa6f19d
Kristine <kteich88@gmail.com>
2018-04-06 18:30:32 -0400

    updated object with example pages to be passed into script.

--- chromeless/sitePage.js
+++ chromeless/sitePage.js
@@ -1,11 +1,11 @@
 module.exports = sitePage = [
 	'/',
-  'blog/',
-  '2016/data-structures/',
+  '/blog/',
+  '/2016/data-structures/',
   '/2016/basics/',
   '/2016/guidance/',
   '/2016/sintra-ditty-rails',
   '/2016/BubbiesBlog/',
   '/2016/the-one-post-to-rule-them-all/',
-  'about/'
+  '/about/'
 ]

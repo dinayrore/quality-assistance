3c951f6e3060e69229b4d5925bc58e025cc4c75f
Kristine <kteich88@gmail.com>
2018-04-06 18:13:50 -0400

    adding chromeless to optional list of test automation.

---
+++ chromeless/README.md
@@ -0,0 +1,24 @@
+# Quality Assistance - Chromeless
+
+Chrome automation made simple. Example shown here is for capturing a full page view.
+
+### Prerequisites
+
+Node.js - https://nodejs.org/en/
+npm - https://www.npmjs.com/get-npm
+
+`npm install chromeless`
+
+### Getting Started
+
+`node screenshotScript.js`
+
+### Documentation
+
+npm chromless - https://www.npmjs.com/package/chromeless
+Chromeless Playground - https://chromeless.netlify.com/
+Github repo - https://github.com/graphcool/chromeless
+
+## Authors
+
+* **Kristine Teichmann**
---
+++ chromeless/screenshotScript.js
@@ -0,0 +1,23 @@
+require('./sitePage')
+
+const { Chromeless } = require('chromeless')
+const fs = require('fs');
+
+let url = 'https://kteich88.github.io';
+
+sitePage.forEach(async function run(page) {
+  const chromeless = new Chromeless()
+    .goto(newUrl + page)
+
+    console.log(page);
+
+  let dir = './screenshots' + page ;
+  if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+  }
+  const screenshot = await chromeless
+    .screenshot('body', { filePath: dir + 'page.png' })
+
+    console.log(screenshot) // prints local file path or S3 url
+  await chromeless.end()
+})
